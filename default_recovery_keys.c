4d8f58d18775ae83ac5a6e62433d585df5c13a74
Ivan Cerrato <ivan@cerrato.biz>
2012-12-23 12:26:53 +0100

    Fix UMS, Fix home key
    Change-Id: I68d5b89ff66d82e71368c84f1a9b734603b7d8ed

--- default_recovery_keys.c
+++ default_recovery_keys.c
@@ -4,7 +4,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
@@ -12,9 +11,9 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
     // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
     if (ui_get_showing_back_button()) {
         return 0;
-        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
+        //return get_allow_toggle_display() && (key_code == KEY_HOMEPAGE || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+    return get_allow_toggle_display() && (key_code == KEY_HOMEPAGE || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -29,7 +28,7 @@ int device_handle_key(int key_code, int visible) {
             case KEY_LEFTSHIFT:
             case KEY_UP:
             case KEY_VOLUMEUP:
-            case KEY_HOME:
+            case KEY_HOMEPAGE:
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
